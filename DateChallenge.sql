4ff60f358500388eb59cdf64553c5bfcb223afc5
Alex <115927556+Renchaku@users.noreply.github.com>
2022-10-18 12:43:18 +0100

    Add files via upload

---
+++ DateChallenge.sql
@@ -0,0 +1,3 @@
+SELECT dbFirstname || ', ' || dbLastname AS "Full Name", ROUND((SYSDATE - dbDateOfBirth)/365, 1) AS "Age" from DUAL, vod_actor
+ORDER BY dbDateOfBirth ASC;
+-- Calculates age of actors
